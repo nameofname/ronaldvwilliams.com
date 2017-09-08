3b38299e99acd42f56a30cb60e2a074660c2f211
nameofname <nameofemail@gmail.com>
2017-09-07 23:21:12 -0400

    game of life page now serves up, styling is a litte nicer

--- index.js
+++ index.js
@@ -8,19 +8,20 @@ app.use(morgan('common'));
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/submodules/the-game-of-life/public/'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/game-of-life', function(request, response) {
-  const html = fs.readFileSync(`${__dirname}/submodules/the-game-of-life/public/life.html`, {encoding: "utf-8"});
-  response.render(html);
-});
+// app.get('/game-of-life', function(request, response) {
+//   // const html = fs.readFileSync(`${__dirname}/submodules/the-game-of-life/public/life.html`, {encoding: "utf"});
+//   response.render('pages/game-of-life');
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
