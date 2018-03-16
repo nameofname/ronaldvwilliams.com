ff439175bd82924d6dcc2af0c2056c82bf02f855
nameofname <nameofemail@gmail.com>
2018-03-16 19:19:10 -0400

    test code in updating the way the char codes are working

--- public/js/ronald.js
+++ public/js/ronald.js
@@ -3,7 +3,8 @@
 
 const { drawFace, drawRadiatingCircles, drawFireWork } = require('./circle');
 const moveSpaceShip = require('./moveSpaceShip');
-const charCodeSequence = require('char-code-sequence');
+// const charCodeSequence = require('char-code-sequence');
+const { listenKeypress, konami } = require('char-code-sequence');
 
 function getTextNodes() {
     let node;
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const nameArr = [ 114, 111, 110, 32, 119, 105, 108, 108, 105, 97, 109, 115];
     const nameClassArr = ['.name-r', '.name-o', '.name-n', null, '.name-w', '.name-i', '.name-l', '.name-l1', '.name-i1', '.name-a', '.name-m', '.name-s'];
-    const nameMatch = charCodeSequence.listen(nameArr, drawFace); // my name
+    const nameMatch = listenKeypress(nameArr, drawFace); // my name
 
     nameMatch.onChange(({ currArr }) => {
         console.log('RONALD : nameArr');
@@ -39,12 +40,27 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // // for testing : 
-    // const oneMatch = charCodeSequence.listen([49, 50, 49, 50, 51], () => console.log('THE ONE TWO THREE THING HAPPENED!!!!!')); // 1, 2, 1, 2, 3
+    // const oneMatch = charCodeSequence.listenKeypress([49, 50, 49, 50, 51], () => console.log('THE ONE TWO THREE THING HAPPENED!!!!!')); // 1, 2, 1, 2, 3
     // oneMatch.onChange(({ currArr }) => {
     //     console.log('let me try it : oneArr');
     //     document.querySelector('.nav').innerText = currArr.map(letter => String.fromCharCode(letter))
     // });
 
     window.der = getTextNodes()
+
+
+    const matchArray = [49, 50, 49, 50, 51]; // 1, 2, 1, 2, 3
+
+
+    const findMatch = listenKeypress([49, 50, 49, 50, 51], (currArr) => {
+        document.querySelector('body').innerText = `You typed the following : ${currArr.map(String.fromCharCode)}`;
+    });
+    
+    findMatch.onChange(({ currArr }) => {
+        console.log('one step closer : ', currArr);
+    });
+    
+    
 });
+
 
